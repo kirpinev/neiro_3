41d2b44bce97a12498fde64641ba418b7015c068
Igor Kirpinev <kirpinevigor@yandex.ru>
2024-11-27 17:48:10 +0300

    fix

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -30,7 +30,7 @@ export const ThxLayout = () => {
       </div>
 
       <div className={appSt.bottomBtnThx}>
-        <ButtonMobile block view="primary" href="">
+        <ButtonMobile block view="primary" href="https://online.alfabank.ru/">
           Вернуться на главный
         </ButtonMobile>
       </div>
