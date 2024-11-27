28b8d0e7b114143a3646a23325d9a0ebee3298a7
Igor Kirpinev <kirpinevigor@yandex.ru>
2024-11-27 11:29:36 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin(), mkcert()],
+  plugins: [react(), vanillaExtractPlugin()],
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
